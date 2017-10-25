844a0dfdd7fca88acfa512c2ad3d33320ac7a247
Scott Babcock <scoba@hotmail.com>
2017-10-25 09:08:57 -0700

    Add path methods for selecting reports directories and subclass names

--- src/main/java/com/nordstrom/common/file/PathUtils.java
+++ src/main/java/com/nordstrom/common/file/PathUtils.java
@@ -6,6 +6,7 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.PathMatcher;
+import java.nio.file.Paths;
 import java.util.Comparator;
 import java.util.Objects;
 import java.util.Optional;
@@ -53,6 +54,64 @@ public class PathUtils {
         throw new AssertionError("PathUtils is a static utility class that cannot be instantiated");
     }
     
+    private static final String SUREFIRE_PATH = "surefire-reports";
+    private static final String FAILSAFE_PATH = "failsafe-reports";
+    
+    public enum ReportsDirectory {
+        
+        SUREFIRE_1("(Test)(.*)", SUREFIRE_PATH),
+        SUREFIRE_2("(.*)(Test)", SUREFIRE_PATH),
+        SUREFIRE_3("(.*)(Tests)", SUREFIRE_PATH),
+        SUREFIRE_4("(.*)(TestCase)", SUREFIRE_PATH),
+        FAILSAFE_1("(IT)(.*)", FAILSAFE_PATH),
+        FAILSAFE_2("(.*)(IT)", FAILSAFE_PATH),
+        FAILSAFE_3("(.*)(ITCase)", FAILSAFE_PATH),
+        ARTIFACT(".*", "artifact-capture");
+        
+        private String regex;
+        private String folder;
+        
+        ReportsDirectory(String regex, String folder) {
+            this.regex = regex;
+            this.folder = folder;
+        }
+        
+        public String getRegEx() {
+            return regex;
+        }
+        
+        public String getFolder() {
+            return folder;
+        }
+        
+        public Path getPath() {
+            return getTargetPath().resolve(folder);
+        }
+        
+        public static ReportsDirectory fromObject(Object obj) {
+            String name = obj.getClass().getSimpleName();
+            for (ReportsDirectory constant : values()) {
+                if (name.matches(constant.regex)) {
+                    return constant;
+                }
+            }
+            return null;
+        }
+        
+        public static Path getPathForObject(Object obj) {
+            ReportsDirectory constant = fromObject(obj);
+            Path path = getTargetPath();
+            if (constant != null) {
+                path = path.resolve(constant.folder);
+            }
+            return path;
+        }
+        
+        private static Path getTargetPath() {
+            return Paths.get(getBaseDir(), "target");
+        }
+    }
+
     /**
      * Get the next available path in sequence for the specified base name and extension in the specified folder.
      * 
@@ -110,4 +169,14 @@ public class PathUtils {
         return targetPath.resolve(newName);
     }
     
+    
+  /**
+   * Get project base directory.
+   * 
+   * @return project base directory
+   */
+  public static String getBaseDir() {
+      Path currentRelativePath = Paths.get(System.getProperty("user.dir"));
+      return currentRelativePath.toAbsolutePath().toString();
+  }
 }
