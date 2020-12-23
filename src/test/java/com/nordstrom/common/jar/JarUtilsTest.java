518587d059129823a9b5227fda901932d60ef17b
Scott Babcock <scoba@hotmail.com>
2020-12-23 12:56:25 -0800

    Add JAR utilities; add missing class description JavaDoc

---
+++ src/test/java/com/nordstrom/common/jar/JarUtilsTest.java
@@ -0,0 +1,25 @@
+package com.nordstrom.common.jar;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import java.io.File;
+import org.testng.annotations.Test;
+
+public class JarUtilsTest {
+    
+    private static String[] CONTEXTS = { "org.testng.annotations.Test", "com.beust.jcommander.JCommander",
+            "org.apache.derby.jdbc.EmbeddedDriver", "com.google.common.base.Charsets" };
+    
+    @Test
+    public void testClasspath() {
+        String result = JarUtils.getClasspath(CONTEXTS);
+        String[] paths = result.split(";");
+        assertEquals(paths.length, CONTEXTS.length, "path entry count mismatch");
+        for (String thisPath : paths) {
+            File file = new File(thisPath);
+            assertTrue(file.exists(), "nonexistent path entry: " + thisPath);
+        }
+    }
+    
+}
