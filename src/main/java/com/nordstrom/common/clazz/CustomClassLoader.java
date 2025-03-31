ffb3c9bd4911d540965985cdb7e2646c0cfbf1ea
Scott Babcock <scoba@hotmail.com>
2025-03-31 16:34:58 -0700

    Add path-based class loader (#39)

---
+++ src/main/java/com/nordstrom/common/clazz/CustomClassLoader.java
@@ -0,0 +1,37 @@
+package com.nordstrom.common.clazz;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+/**
+ * This class implements a custom class loader initialized with a standard class path specification.
+ */
+public class CustomClassLoader extends ClassLoader {
+
+    private final URL[] pathUrls;
+
+    public CustomClassLoader(String classpath) {
+        pathUrls = Arrays.stream(classpath.split(File.pathSeparator)).map(entry -> {
+            try {
+                return Paths.get(entry).toUri().toURL();
+            } catch (MalformedURLException e) {
+                throw new UncheckedIOException("Invalid classpath entry: " + entry, e);
+            }
+        }).toArray(URL[]::new);
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        try (URLClassLoader classLoader = new URLClassLoader(pathUrls)) {
+            return classLoader.loadClass(name);
+        } catch (IOException e) {
+            throw new ClassNotFoundException("Failed loading class: " + name, e);
+        }
+    }
+}
